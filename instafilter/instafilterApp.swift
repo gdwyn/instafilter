e483737bb5b89bbf52aa5d51313eb14a07d6a075
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2024-04-12 18:59:06 +0100

    Initial Commit

---
+++ instafilter/instafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  instafilterApp.swift
+//  instafilter
+//
+//  Created by Godwin IE on 12/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct instafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
