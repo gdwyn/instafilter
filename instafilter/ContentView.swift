faf1ebb5180efa6da27120308023954d4d736587
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2024-04-13 16:22:21 +0100

    filter and filter keys

--- instafilter/ContentView.swift
+++ instafilter/ContentView.swift
@@ -5,17 +5,53 @@
 //  Created by Godwin IE on 12/04/2024.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
+    @State private var image: Image?
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            image?
+                .resizable()
+                .scaledToFit()
         }
         .padding()
+        .onAppear(){
+            loadImage()
+        }
+    }
+    
+    func loadImage() {
+        let inputImage = UIImage(resource: .example)
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        let currentFilter = CIFilter.pixellate()
+        
+        currentFilter.inputImage = beginImage
+        
+        //setting the value to apply to current filter
+        let amount = 1
+        
+        let inputKeys = currentFilter.inputKeys
+        
+        //applying the amount to the filter based on the filter selected
+        if inputKeys.contains (kCIInputIntensityKey) { currentFilter.setValue (amount, forKey:
+        kCIInputIntensityKey) }
+        if inputKeys.contains (kCIInputRadiusKey) { currentFilter.setValue (amount * 200, forKey:
+        kCIInputRadiusKey) }
+        if inputKeys.contains (kCIInputScaleKey) { currentFilter.setValue(amount * 10, forKey:
+        kCIInputScaleKey) }
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+        
+        let uiImage = UIImage(cgImage: cgImage)
+        
+        image = Image(uiImage: uiImage)
     }
 }
 
