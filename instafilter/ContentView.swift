6379629b58716e95a37a418e5c503db6886707f6
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2024-04-18 02:09:09 +0100

    select photos

--- instafilter/ContentView.swift
+++ instafilter/ContentView.swift
@@ -5,53 +5,41 @@
 //  Created by Godwin IE on 12/04/2024.
 //
 
-import CoreImage
-import CoreImage.CIFilterBuiltins
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
-    @State private var image: Image?
+    @State private var pickerItems = [PhotosPickerItem]()
+    @State private var selectedImages = [Image]()
+    
     var body: some View {
         VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-        }
-        .padding()
-        .onAppear(){
-            loadImage()
-        }
-    }
-    
-    func loadImage() {
-        let inputImage = UIImage(resource: .example)
-        let beginImage = CIImage(image: inputImage)
-        
-        let context = CIContext()
-        let currentFilter = CIFilter.pixellate()
-        
-        currentFilter.inputImage = beginImage
-        
-        //setting the value to apply to current filter
-        let amount = 1
-        
-        let inputKeys = currentFilter.inputKeys
-        
-        //applying the amount to the filter based on the filter selected
-        if inputKeys.contains (kCIInputIntensityKey) { currentFilter.setValue (amount, forKey:
-        kCIInputIntensityKey) }
-        if inputKeys.contains (kCIInputRadiusKey) { currentFilter.setValue (amount * 200, forKey:
-        kCIInputRadiusKey) }
-        if inputKeys.contains (kCIInputScaleKey) { currentFilter.setValue(amount * 10, forKey:
-        kCIInputScaleKey) }
-        
-        guard let outputImage = currentFilter.outputImage else { return }
-        
-        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
-        
-        let uiImage = UIImage(cgImage: cgImage)
-        
-        image = Image(uiImage: uiImage)
+            PhotosPicker(selection: $pickerItems,
+                         maxSelectionCount: 3,
+                         matching: .any(of: [.images, .screenshots, .not(.videos)])) {
+                Label("Select pictures", systemImage: "photo")
+            }
+            
+            ScrollView {
+                ForEach (0..<selectedImages.count, id: \.self) {i in
+                    selectedImages[i]
+                        .resizable()
+                        .scaledToFit()
+                }
+            } // scrollview
+            
+        } // Vstack
+        .onChange(of: pickerItems) {
+            Task {
+                selectedImages.removeAll()
+                
+                for item in pickerItems {
+                    if let loadedImage = try await item.loadTransferable(type: Image.self) {
+                        selectedImages.append(loadedImage)
+                    }
+                }
+            }
+        } // onChange
     }
 }
 
