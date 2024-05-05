cd082d2cf3848cc5a57db1d170ade4ff7d78b252
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2024-05-05 14:00:27 +0100

    import image

--- instafilter/ContentView.swift
+++ instafilter/ContentView.swift
@@ -5,24 +5,30 @@
 //  Created by Godwin IE on 24/04/2024.
 //
 
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
     @State private var processedImage: Image?
     @State private var filterIntensity = 0.5
+    @State private var selectedItem: PhotosPickerItem?
     
     var body: some View {
         NavigationStack{
             VStack{
                 Spacer()
                 
-                if let processedImage{
-                    processedImage
-                        .resizable()
-                        .scaledToFit()
-                } else {
-                    ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Tap to add photo"))
-                }
+                PhotosPicker(selection: $selectedItem) {
+                    if let processedImage{
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Tap to add photo"))
+                    }
+                } //photospicker
+                .buttonStyle(.plain)
+                .onChange(of: selectedItem, loadImage)
                 
                 Spacer()
                 
@@ -47,7 +53,17 @@ struct ContentView: View {
     
     func changeFilter() {
         
-    }
+    } //change filter
+    
+    func loadImage() {
+        Task {
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else {return}
+            
+            guard let inputImage = UIImage(data: imageData) else {return}
+            
+            //more code to come
+        }
+    } //load image
 }
 
 #Preview {
